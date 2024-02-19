57ae58e3f3f35076356b4f34819d50c877f3d3be
James Park rtb23126 <james.park.2023@uni.strath.ac.uk>
2024-02-19 11:38:44 +0000

    Player class tested

---
+++ out/production/cs207-bulls-and-cows/README.md
@@ -0,0 +1,19 @@
+# CS207 Group 5 - Bulls and Cows
+
+Welcome to the CS207 Group 5 Git Repository for the Bulls and Cows Assignment!
+
+# Members:
+
+- James Park
+- Robert Nairn
+- Sean McIlroy
+- Shaun Dempsey
+- Stuart Gordon
+
+## Sprint One
+
+1. As a player I want to be able to request a secret code so I can try to  decipher it
+2. As a player I want to be able to enter a guess so I can decipher the secret code
+3. As a player I want to be able to undo a letter/number so I can try to decipher the secret code
+
+- [ ] 
--- tests/PlayerTests.java
+++ tests/PlayerTests.java
@@ -4,12 +4,35 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class PlayerTests {
     @Test
     public void testPlayer(){
         Player player = new Player();
         assertNotNull(player);
+        assertNull(player.getUsername());
+        assertEquals( 0, player.getNumberOfBulls());
+        assertEquals( 0, player.getNumberOfCows());
+        assertEquals(0, player.getCodesAttempted());
+        assertEquals(0, player.getCodesDeciphered());
+
+        player.setUsername("JDoe");
+        player.setCodesAttempted(3);
+        player.setCodesDeciphered(1);
+        player.setNumberOfBulls(3);
+        player.setNumberOfCows(2);
+
+        assertEquals("JDoe", player.getUsername());
+        assertEquals(3, player.getCodesAttempted());
+        assertEquals(1, player.getCodesDeciphered());
+        assertEquals( 3, player.getNumberOfBulls());
+        assertEquals( 2, player.getNumberOfCows());
+
+        player.incrementCodesAttempted();
+        player.incrementCodesDeciphered();
+        player.incrementCodesDeciphered();
+
+        assertEquals(4, player.getCodesAttempted());
+        assertEquals(3, player.getCodesDeciphered());
     }
 
 }
