61daff57c0177bdf9047c666036f9f441ae686c2
Robert Nairn <robert.nairn.2022@uni.strath.ac.uk>
2024-02-19 11:32:56 +0000

    framework for Game, NumbersCode and SecretCode

--- Game.java
+++ Game.java
@@ -1,3 +1,49 @@
-
 public class Game {
+    private Player playerGameMapping;
+    private Player currentPlayer;
+
+    public void Game(Player P, String CodeType){
+
+    }
+
+    public void Game(Player P){
+
+    }
+
+    public void getHint(){
+
+    }
+
+    public Player loadPlayer(){
+        return currentPlayer;
+    }
+
+    public void playGame(){
+
+    }
+
+    public void requestCode(){
+
+    }
+
+    public void enterGuess(){
+
+    }
+
+    public void undoGuess(){
+
+    }
+
+    public void saveGame(){
+
+    }
+
+    public void loadGame(){
+
+    }
+
+    public void showSolution(){
+        return;
+    }
+
 }
