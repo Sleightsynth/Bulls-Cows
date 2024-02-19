1e888881dd0417b895c8d112fcf5d48dafc15fb1
James Park rtb23126 <james.park.2023@uni.strath.ac.uk>
2024-02-19 11:01:15 +0000

    PlayerClass

--- Player.java
+++ Player.java
@@ -1,3 +1,86 @@
-
 public class Player {
+    
+    // ATTRIBUTES
+    // ----------
+
+    private String username;
+    
+    private int numberOfBulls, numberOfCows, codesAttempted, codesDeciphered;
+
+    // CONSTRUCTERS
+    // ------------
+    
+    public Player(){}
+
+    public Player(
+        String username, int numberOfBulls, int numberOfCows, int codesAttempted, int codesDeciphered
+    ){
+        this.username = username;
+        this.numberOfBulls = numberOfBulls;
+        this.numberOfCows = numberOfCows;
+        this.codesAttempted = codesAttempted;
+        this.codesDeciphered = codesDeciphered;
+    }
+
+    // GETTERS
+    // -------
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getNumberOfBulls() {
+        return numberOfBulls;
+    }
+    
+    public int getNumberOfCows() {
+        return numberOfCows;
+    }
+    
+    public int getCodesDeciphered() {
+        return codesDeciphered;
+    }
+    
+    public int getCodesAttempted() {
+        return codesAttempted;
+    }
+
+    // SETTERS
+    // -------
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setNumberOfBulls(int numberOfBulls) {
+        this.numberOfBulls = numberOfBulls;
+    }
+
+    public void setNumberOfCows(int numberOfCows) {
+        this.numberOfCows = numberOfCows;
+    }
+    
+    public void setCodesDeciphered(int codesDeciphered) {
+        this.codesDeciphered = codesDeciphered;
+    }
+
+    public void incrementCodesDeciphered(){
+        ++this.codesDeciphered;
+    }
+    }
+
+    public void incrementCodesAttempted(){
+        ++this.codesAttempted;
+    }
+    
+    public void setCodesAttempted(int codesAttempted) {
+        this.codesAttempted = codesAttempted;
+    }
+
+    // METHODS
+    // -------
+
+
+
+
 }
