a3b0ca9f1e4a61fde8a64aa12350650c09f049a7
Robert Nairn <robert.nairn.2022@uni.strath.ac.uk>
2024-02-28 13:28:36 +0000

    letterCode reading from file

--- main/LetterCode.java
+++ main/LetterCode.java
@@ -1,4 +1,31 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.NoSuchFileException;
+import java.util.Scanner;
+
 public class LetterCode extends SecretCode{
+
+    public void getCode() {
+        try {
+            File file = new File("LetterCode.txt");
+
+            Scanner scanner = new Scanner(file);
+
+            if (scanner.hasNext()) {
+                String secretWord = scanner.next();
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
 }
